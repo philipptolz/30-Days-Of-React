73b8880cd48924cd750181d578ed03628e5db0ef
philipptolz <philto04@gmail.com>
2022-12-23 21:01:35 +0100

    chore: day-01 level13 changed

--- solutions/day-01/level13.js
+++ solutions/day-01/level13.js
@@ -7,38 +7,45 @@ const products = [
   { product: "tea", price: "" },
 ];
 
-products.forEach(product => {
-    let price;
-    if (typeof product.price != "number") {
-        price = "unknown";
-    } else {
-        price = product.price;
-    }
+products.forEach((product) => {
+  let price;
+  if (typeof product.price != "number") {
+    price = "unknown";
+  } else {
+    price = product.price;
+  }
 
-    console.log("The price of "  + product.product + " is " +  price + " euros.")
-})
-
+  console.log("The price of " + product.product + " is " + price + " euros.");
+});
 
 let sum = 0;
 
-products.forEach(product => {
-    if (!(typeof product.price != "number")) {
-        sum += product.price;
-    }
-})
+products.forEach((product) => {
+  if (!(typeof product.price != "number")) {
+    sum += product.price;
+  }
+});
 
 console.log("The sum of prices: " + sum + " euros.");
 
-const priceArray = products.map(product => product.price)
+const priceArray = products.map((product) => product.price);
 
-const filteredPriceArray = priceArray.filter(prize => typeof prize === "number")
+const filteredPriceArray = priceArray.filter(
+  (prize) => typeof prize === "number"
+);
 
-const priceArraySum = filteredPriceArray.reduce((prize, sum) =>  sum += prize)
+const priceArraySum = filteredPriceArray.reduce((prize, sum) => (sum += prize));
 
-const firstWithoutPrice = products.find(product => typeof product.price != "number")
+const firstWithoutPrice = products.find(
+  (product) => typeof product.price != "number"
+);
 
-const firstWithoutPriceIndex = products.findIndex(product => typeof product.price != "number")
+const firstWithoutPriceIndex = products.findIndex(
+  (product) => typeof product.price != "number"
+);
 
-console.log("The sum of prices: " + priceArraySum + " euros.")
-console.log("The first without price: " + firstWithoutPrice.product)
-console.log("The first without price(Index): " + firstWithoutPriceIndex)
+console.log("The sum of prices: " + priceArraySum + " euros.");
+
+console.log("The first without price: " + firstWithoutPrice.product);
+
+console.log("The first without price(Index): " + firstWithoutPriceIndex);
